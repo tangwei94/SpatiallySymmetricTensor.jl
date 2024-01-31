7650c7bb35b1642faf68815cae4a07b9d6f90531
Wei Tang <wei.tang.nju@gmail.com>
2024-01-31 14:38:11 +0100

    doc

--- src/spatial_operations.jl
+++ src/spatial_operations.jl
@@ -1,3 +1,8 @@
+"""
+    mapping_table(T::AbstractTensorMap)
+
+    - generate a mapping table for the free parameters in a symmetric tensor. Constructs the mapping between the linear space of the free parameters and the symmetric tensor.
+"""
 function mapping_table(T::AbstractTensorMap)
     res = Tuple{Int, Int}[]
     for (ix, v) in enumerate(T.data.values)
@@ -12,26 +17,51 @@ function get_data_by_index(T::AbstractTensorMap, ix::Int; _mapping_table=mapping
     T.data.values[_mapping_table[ix][1]][_mapping_table[ix][2]]
 end
 
-# modify the free parameters in a tensor
+"""
+    set_data_by_vector!(T::AbstractTensorMap, _values; _mapping_table=mapping_table(T))
+
+    - modify the free parameters in a symmetric tensor by _values
+    - map a vector that lives the linear space of the free parameters to a symmetric tensor. The mapping is defined by the symmetric structure of the symmetric tensor 
+"""
 function set_data_by_vector!(T::AbstractTensorMap, _values; _mapping_table=mapping_table(T))
     for ix in eachindex(_values)
         T.data.values[_mapping_table[ix][1]][_mapping_table[ix][2]] = _values[ix]
     end
     T
 end
-# generate a new tensor which has the same symmetry structure as T, but with free parameters set by _values
+
+"""
+    set_data_by_vector(T::AbstractTensorMap, _values; _mapping_table=mapping_table(T))
+
+    - generate a new tensor which has the same symmetry structure as T, but with free parameters set by `_values`
+    - map a vector that lives the linear space of the free parameters to a symmetric tensor. The mapping is defined by the symmetric structure of the symmetric tensor 
+"""
 function set_data_by_vector(T::AbstractTensorMap, _values; _mapping_table=mapping_table(T))
     T1 = zero(T) 
     set_data_by_vector!(T1, _values; _mapping_table=_mapping_table)
 end
 
-# convert the free parameters in a tensor to a vector, in the same order as set_data_by_vector
+"""
+    Base.vec(T::AbstractTensorMap; _mapping_table=mapping_table(T))
+
+    - convert the free parameters in a tensor to a vector, in the same order as set_data_by_vector
+
+    - map the symmetric tensor to a vector that lives the linear space of the free parameters
+"""
 function Base.vec(T::AbstractTensorMap; _mapping_table=mapping_table(T))
     map(eachindex(_mapping_table)) do ix
         T.data.values[_mapping_table[ix][1]][_mapping_table[ix][2]]
     end
 end
 
+"""
+    selector(T::AbstractTensorMap, _condition; _mapping_table=mapping_table(T))
+
+    - construct a projector in the linear space of the free parameters of T
+    - the subspace corresponds to the symmetric tensors that satisfy _condition
+returreturnn
+    - `_condition` is a function that takes two arguments `f1` and `f2`, which are the `FusionTrees` of `T`. The function return boolean values. For the explanation of `TensorKit.FusionTrees`, see https://jutho.github.io/TensorKit.jl/latest/man/sectors/.  
+"""
 function selector(T::AbstractTensorMap, _condition; _mapping_table=mapping_table(T))
     num_paras = length(_mapping_table)
     P = zeros(ComplexF64, num_paras, num_paras)
@@ -47,6 +77,14 @@ function selector(T::AbstractTensorMap, _condition; _mapping_table=mapping_table
     return P[:, findall(ix->P[ix, ix]==1, 1:num_paras)]
 end
 
+"""
+    spatial_operation(T::AbstractTensorMap, permutations; _mapping_table=mapping_table(T))
+
+    - When you do a spatial operation to a symmetric tensor, you map a symmetric tensor to another symmetric tensor. 
+    - This mapping can be represented by a matrix defined in the linear space of the free parameters of the symmetric tensor.
+    - The function `spatial_operation` returns this matrix.
+
+"""
 function spatial_operation(T::AbstractTensorMap, permutations; _mapping_table=mapping_table(T))
     num_paras = length(_mapping_table)
     M = zeros(ComplexF64, num_paras, num_paras)
