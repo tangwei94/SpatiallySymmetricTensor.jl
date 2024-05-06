7eb423cdcb5e4a18be7fb89ad525913c4a1a27c5
Wei Tang <tangwei@smail.nju.edu.cn>
2024-05-06 15:38:28 +0200

    update

--- long_range_RVB.jl
+++ long_range_RVB.jl
@@ -11,35 +11,38 @@ P = SU2Space(1//2=>1)
 T = TensorMap(zeros, ComplexF64, P, V^4)
 
 T_1_3_A1 = IPEPSC6v.T_1_3_A1()
-T_3_1_A1 = IPEPSC6v.T_1_3_A1()
+T_3_1_A1 = IPEPSC6v.T_3_1_A1()
+
+convert(Array, 2*sqrt(2)*T_1_3_A1)
+convert(Array, sqrt(3)*4*T_3_1_A1)
 
 λ = parse(Float64, ARGS[1])
-λ = 0.35
-Tfull, TA, TB = IPEPSC6v.long_range_RVB(λ)
-@show IPEPSC6v.mpo_hermicity(Tfull)
-@show IPEPSC6v.mpo_normality(Tfull)
+λ = 0.14
+for χ in 120:20:200
+    Tfull, TA, TB = IPEPSC6v.long_range_RVB(λ)
+    @show IPEPSC6v.mpo_hermicity(Tfull)
+    @show IPEPSC6v.mpo_normality(Tfull)
 
-ψi = InfiniteMPS([fuse(V'*V)], [fuse(V'*V)])
+    ψi = InfiniteMPS([fuse(V'*V)], [fuse(V'*V)])
+    Tfull, TA, TB, A, B = IPEPSC6v.long_range_RVB(λ)
+    𝕋full = DenseMPO([Tfull]) 
+    let ψ1 = ψi, ψ2 = ψi
+        ψ1 = ψi
+        for ix in 1:25 
+            ψ1 = changebonds(𝕋full * ψ1, SvdCut(truncdim(χ))) 
+            @show ix, domain(ψ1.CR[1]) 
+        end 
+        ψ2, _, _ = leading_boundary(ψ1, 𝕋full, VUMPS(tol_galerkin=1e-12, maxiter=1000)); 
+        @save "data/long_range_RVB_lambda$(λ)_chi$(χ).jld2" ψ1 ψ2
+    end
 
-Tfull, TA, TB, A, B = IPEPSC6v.long_range_RVB(λ)
+    @load "data/long_range_RVB_lambda$(λ)_chi$(χ).jld2" ψ1 ψ2
+    # transfer matrix
+    ψA = ψ2.AL[1]
+    Etot, E1, E2, E3, E4 = IPEPSC6v.long_range_RVB_energy(Tfull, A, TB, ψA);
+    @show Etot, E1, E2, E3, E4
 
-𝕋full = DenseMPO([Tfull]) 
-let ψ1 = ψi, ψ2 = ψi
-    ψ1 = ψi
-    for ix in 1:20 
-        ψ1 = changebonds(𝕋full * ψ1, SvdCut(truncdim(200))) 
-        @show ix, domain(ψ1.CR[1]) 
-    end 
-    ψ2, _, _ = leading_boundary(ψ1, 𝕋full, VUMPS(tol_galerkin=1e-12, maxiter=1000)); 
-    @save "data/long_range_RVB_lambda$(λ)_chi200.jld2" ψ1 ψ2
+    io = open("tmpdata.txt", "a");
+    write(io, "$(λ) $(χ) $(E1) $(E2) $(E3) $(E4) $(Etot)\n")
+    close(io)
 end
-
-@load "data/long_range_RVB_lambda$(λ)_chi200.jld2" ψ1 ψ2
-# transfer matrix
-ψA = ψ2.AL[1]
-Etot, E1, E2, E3, E4 = IPEPSC6v.long_range_RVB_energy(Tfull, A, TB, ψA);
-@show Etot, E1, E2, E3, E4
-
-io = open("tmpdata.txt", "a");
-write(io, "$(λ) $(E1) $(E2) $(E3) $(E4) $(Etot)\n")
-close(io)
