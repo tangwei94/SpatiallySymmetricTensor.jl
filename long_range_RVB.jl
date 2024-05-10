4a4222bf05519ce83213f159c5eb3c97458837c1
Wei Tang <tangwei@smail.nju.edu.cn>
2024-05-10 10:56:41 +0200

    use itebd2 to initialize

--- long_range_RVB.jl
+++ long_range_RVB.jl
@@ -17,15 +17,14 @@ convert(Array, 2*sqrt(2)*T_1_3_A1)
 convert(Array, sqrt(3)*4*T_3_1_A1)
 
 λ = parse(Float64, ARGS[1])
-λ = 0.14
-for χ in 120:20:200
+for χ in 100:20:200
     Tfull, TA, TB = IPEPSC6v.long_range_RVB(λ)
     @show IPEPSC6v.mpo_hermicity(Tfull)
     @show IPEPSC6v.mpo_normality(Tfull)
 
-    ψi = InfiniteMPS([fuse(V'*V)], [fuse(V'*V)])
+    ψi = InfiniteMPS([fuse(V'*V), fuse(V'*V)], [fuse(V'*V), fuse(V'*V)])
     Tfull, TA, TB, A, B = IPEPSC6v.long_range_RVB(λ)
-    𝕋full = DenseMPO([Tfull]) 
+    𝕋full = DenseMPO([Tfull, Tfull]) 
     let ψ1 = ψi, ψ2 = ψi
         ψ1 = ψi
         for ix in 1:25 
@@ -33,16 +32,16 @@ for χ in 120:20:200
             @show ix, domain(ψ1.CR[1]) 
         end 
         ψ2, _, _ = leading_boundary(ψ1, 𝕋full, VUMPS(tol_galerkin=1e-12, maxiter=1000)); 
-        @save "data/long_range_RVB_lambda$(λ)_chi$(χ).jld2" ψ1 ψ2
+        @save "data/itebd2_long_range_RVB_lambda$(λ)_chi$(χ).jld2" ψ1 ψ2
     end
 
-    @load "data/long_range_RVB_lambda$(λ)_chi$(χ).jld2" ψ1 ψ2
+    @load "data/itebd2_long_range_RVB_lambda$(λ)_chi$(χ).jld2" ψ1 ψ2
     # transfer matrix
     ψA = ψ2.AL[1]
     Etot, E1, E2, E3, E4 = IPEPSC6v.long_range_RVB_energy(Tfull, A, TB, ψA);
     @show Etot, E1, E2, E3, E4
 
-    io = open("tmpdata.txt", "a");
+    io = open("itebd2_tmpdata.txt", "a");
     write(io, "$(λ) $(χ) $(E1) $(E2) $(E3) $(E4) $(Etot)\n")
     close(io)
 end
